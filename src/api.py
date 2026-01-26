e156bcc04bb669db326c8dc36c2745825c5dd95b
Rue Ire <reerine20@msn.com>
2026-01-26 20:35:28 +0200

    add, edit: api, transform, gitignore, requirements

---
+++ src/api.py
@@ -0,0 +1,42 @@
+
+import requests
+# import pandas as pd
+from pandas import json_normalize
+from pathlib import Path
+
+def get_data():
+    all_data = []
+    page = 0
+
+    try:
+        while True:
+            response = requests.get(f'https://digi-api.com/api/v1/digimon?page={page}')
+            response.raise_for_status()
+
+            data = response.json()
+            page_data = data['content']
+
+            if data["pageable"]["nextPage"] == "":
+                break
+
+            # if isinstance(data, dict):
+                # print(data.keys()) => dict_keys(['content', 'pageable'])
+
+            all_data.extend(page_data)
+            page += 1
+
+        df = json_normalize(all_data)
+
+        # path src/raw
+        dir = Path(__file__).resolve().parent / "raw"
+        dir.mkdir(parents=True, exist_ok=True)   
+
+        file_path = dir/'digimon.csv'
+        df.to_csv(file_path, index=False, encoding='utf-8')
+
+        print(f'{len(all_data)} rows saved to src/raw')
+
+    except Exception as e: 
+        print('error at api request: ', e)
+
+get_data()
---
+++ src/transform.py
@@ -0,0 +1,14 @@
+from operator import index
+import pandas as pd
+from pathlib import Path
+
+
+dir = Path(__file__).resolve().parent / "raw"
+file_path = dir/'digimon.csv'
+
+# Read the CSV file
+data = pd.read_csv(file_path)
+main_data = data[['id', 'name']]
+
+# View the first 5 rows
+print(main_data.head(10).to_string(index=False))
